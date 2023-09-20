e75a32d5c1d20ef97cc2c0c32e83550f7504fc75
Matheus Duarte <myduarte1@hotmail.com>
2023-09-20 08:18:27 -0300

    DB update, minor updates on the pages and register page with more modules

--- app/models.py
+++ app/models.py
@@ -7,3 +7,10 @@ class Usuario(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+class Perfil(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idade = db.Column(db.Integer)
+    peso = db.Column(db.Float)
+    sexo = db.Column(db.String(10))
+    alergias = db.Column(db.String(200))
+    dieta = db.Column(db.String(20))
